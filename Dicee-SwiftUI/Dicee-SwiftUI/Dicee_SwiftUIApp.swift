6f64f0f3f4dfa73a4c7a1d1ae9d046fa00b87c96
Robert Melton <116193054+rsmelton@users.noreply.github.com>
2023-09-18 13:45:26 -0500

    - Added Dicee SwiftUI files

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Scott Melton on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
